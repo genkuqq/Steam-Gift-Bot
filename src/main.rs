3821055db770ff062f952b8ad77dadf97fc1d0f6
genkuqq <koraygench@gmail.com>
2024-10-18 10:14:39 +0300

    add: config tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+use serde::Deserialize;
+use toml;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    username: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let toml_file = fs::read_to_string("config.toml").expect("Error: Reading config file.");
+    let config: Config = toml::from_str(&toml_file).expect("Error: Parsing TOML file.");
+    println!("{:?}", config.username);
 }
